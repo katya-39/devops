fb319c9bc2c2baba135e98cec34744ff87eef89b
Katya <Katyashulman@gmail.com>
2025-02-22 15:04:00 +0200

    avoda

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if a parameter is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <number>"
+  exit 1
+fi
+
+# Assign the parameter to a variable
+number=$1
+
+# Check if the number is even or odd
+if [ $((number % 2)) -eq 0 ]; then
+  echo "The number $number is even."
+else
+  echo "The number $number is odd."
+fi
